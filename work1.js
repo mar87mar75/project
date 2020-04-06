17ca0104c09488ef6e2466977f8af610da329ae2
Denis <branku14@mail.ru>
2020-04-06 19:03:04 +0300

    first commit

---
+++ work1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var leftBorderWidth = 1; // first
+let second = 2; // second
+const pi = 3.14;
+//console.log(second);
+// первый шаг
+// first 
+//alert(`Hello`);
+
+// console.log(4/0);
+//let photo = ['photo1','photo2','photo3'];
+//console.log(photo[2]);
+//alert(`hello`);
+//confirm('ты?');
+let hoay = prompt('Сколько вам лет?','10');
+console.log(typeof(pi));
